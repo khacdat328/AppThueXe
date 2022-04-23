1eb77cbd4070f41037524820414650146bf0442a
TrungPhamDac <84670001+TrungPhamDac@users.noreply.github.com>
2022-04-24 02:22:35 +0700

    create new cars_layout

--- app/src/main/java/com/example/thuexe/MainActivity.kt
+++ app/src/main/java/com/example/thuexe/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.thuexe
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var searchText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        searchText = findViewById(R.id.searchText)
+
+        var temp = arrayOf<String>("nextPage", "aaa", "new")
+
+        searchText.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                val text: String = searchText.text.toString()
+                for(i in temp.indices){
+                    if( text?.equals(temp)){
+                        val intent = Intent(this@MainActivity, carsActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/thuexe/carsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thuexe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class carsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cars_layout)
+    }
+}
