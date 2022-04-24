9a5152b9a4b98304c3e6ba13e8c9199b9abd3d4e
TrungPhamDac <84670001+TrungPhamDac@users.noreply.github.com>
2022-04-24 17:05:02 +0700

    update carsActivity.kt

--- app/src/main/java/com/example/thuexe/carsActivity.kt
+++ app/src/main/java/com/example/thuexe/carsActivity.kt
@@ -1,11 +1,20 @@
 package com.example.thuexe
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class carsActivity : AppCompatActivity() {
+    private lateinit var homeTitle: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cars_layout)
+        homeTitle = findViewById(R.id.Home_Title)
+        homeTitle.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
