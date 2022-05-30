b7ccc6e8d5fc39568b47e669ecce9448e3acce9f
khacdat328 <85425062+khacdat328@users.noreply.github.com>
2022-05-31 01:08:22 +0700

    Admin Dashboard

--- app/src/main/java/com/example/thuexe/adminDashboard.kt
+++ app/src/main/java/com/example/thuexe/adminDashboard.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class adminDashBoard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_dash_board)
+        setContentView(R.layout.activity_admin_dashboard)
     }
 }
