de2ce47e1fa738f9a9fbf228af0aee45ff5f0e96
TrungPhamDac <84670001+TrungPhamDac@users.noreply.github.com>
2022-06-12 17:43:24 +0700

    contract layout

---
+++ app/src/main/java/com/example/thuexe/contractActivity.kt
@@ -0,0 +1,13 @@
+package com.example.thuexe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class contractActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contract_layout)
+
+
+    }
+}
