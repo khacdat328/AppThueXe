6a2ea3c027b904d233a14d9b1473ef09943a2415
khacdat328 <85425062+khacdat328@users.noreply.github.com>
2022-06-14 21:34:25 +0700

    CarManagement

---
+++ app/src/main/java/Model/CarManagement.kt
@@ -0,0 +1,13 @@
+package Model
+
+class CarManagement(
+    var name: String,
+    var gia: Int,
+    var Bienso: String,
+    var soghe: Int,
+    var phankhuc: String,
+    var vitri: String,
+    var Img: String
+) {
+    constructor() : this("", 0, "", 0, "", "", "")
+}
