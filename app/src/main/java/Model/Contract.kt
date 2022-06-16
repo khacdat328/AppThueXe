66ab02f7fc61cc78937909d8261ef36735d2e9f9
khacdat328 <85425062+khacdat328@users.noreply.github.com>
2022-06-17 02:22:00 +0700

    contract Listview

---
+++ app/src/main/java/Model/Contract.kt
@@ -0,0 +1,9 @@
+package Model
+
+class Contract(
+    var clientName: String,
+    var phoneNumber: String,
+    var carName: String,
+    var Bienso: String,
+
+)
--- app/src/main/java/com/example/thuexe/QuanLyHopDong.kt
+++ app/src/main/java/com/example/thuexe/QuanLyHopDong.kt
@@ -1,11 +1,37 @@
 package com.example.thuexe
 
+import Adapter.adminContractAdapter
+import Model.Contract
+import Model.hopdong
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.thuexe.databinding.ActivityQuanLyHopDongBinding
+import com.example.thuexe.databinding.ActivityQuanLyKhachHangBinding
+import kotlinx.android.synthetic.main.quan_ly_hop_dong_layout.*
 
 class QuanLyHopDong : AppCompatActivity() {
+
+    private lateinit var binding1: ActivityQuanLyHopDongBinding
+    private lateinit var contractArrayList: ArrayList<Contract>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding1 = ActivityQuanLyHopDongBinding.inflate(layoutInflater)
         setContentView(R.layout.quan_ly_hop_dong_layout)
+
+        val clientName =
+            arrayOf("Pham Dac Trung", "Huynh Thanh Sang", "Nguyen Phu Cuong", "Chau Khac Dat","Chau Khac Dat")
+
+        val phoneNumber = arrayOf("0123456789", "01346789852", "01346789852", "01346789852","01346789852")
+        val carName = arrayOf("Lamborghini", "Huayra", "Air Blade", "Mercedes","Mercedes")
+        val carPlate = arrayOf("81P1-11590", "81P1-11590", "81P1-11590", "81P1-11590","81P1-11590")
+
+        contractArrayList = ArrayList()
+        for(i in clientName.indices){
+            val contract = Contract(clientName[i],phoneNumber[i],carName[i],carPlate[i])
+            contractArrayList.add(contract)
+        }
+        
+        adminContractListview.adapter = adminContractAdapter(this, contractArrayList)
     }
 }
